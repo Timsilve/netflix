446d2fb4c1366c635c0bb6cea45cc8e7e806a132
timsilve <timsilva112.0@gmail.com>
2025-06-08 20:08:48 +0100

    working on Row

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [likes, setLikes] = useState(false);
   useEffect(() => {
     axios.get(fetchUrl).then((response) => {
       setMovies(response.data.results);
@@ -28,6 +29,13 @@ function Row({ title, fetchUrl }) {
               <div className="absolute hover:bg-black/80 top-0 left-0 w-full h-full opacity-0 duration-200 ease-in  delay-75 hover:opacity-100 text-white transition-all transform-d">
                 <p className="whitespace-normal text-xs md:text-sm font-bold justify-center flex items-center h-full text-center">
                   {item?.title}
+                </p>
+                <p>
+                  {likes ? (
+                    <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                  ) : (
+                    <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                  )}
                 </p>
               </div>
             </div>
